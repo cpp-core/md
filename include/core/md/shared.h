3bce152d072999e9fa5ecde013ff6e47d1a6d54a
Mark Melton <mark.melton@notlem.com>
2024-01-21 13:53:53 -0500

    Add shared_data

--- include/core/md/shared.h
+++ include/core/md/shared.h
@@ -4,71 +4,76 @@
 #pragma once
 #include <memory>
 
-#include <mdspan/mdspan.hpp>
+#include "span.h"
 
 namespace core::md {
 namespace detail {
 
-template<class ElementType>
+template <class ElementType>
 struct shared_ownership_handle {
     std::shared_ptr<ElementType> base;
     size_t offset;
 };
 
-template<class ElementType>
+template <class ElementType>
 auto make_shared_ownership_handle(size_t n, size_t offset = 0) {
     return shared_ownership_handle<ElementType>{std::make_shared<ElementType>(n), offset};
 }
 
-template<std::integral... Sizes>
+template <std::integral... Sizes>
 constexpr size_t product_dynamic_extents(Sizes... sizes) {
     std::array<size_t, sizeof...(Sizes)> arr{static_cast<size_t>(sizes)...};
     size_t n{1};
     for (auto elem : arr)
-	n *= elem;
+        n *= elem;
     return n;
 }
 
-template<class ElementType>
+template <class ElementType>
 struct shared_accessor {
     using offset_policy = shared_accessor;
     using element_type = ElementType;
-    using reference = ElementType&;
+    using reference = ElementType &;
     using data_handle_type = shared_ownership_handle<ElementType>;
-    
+
     constexpr shared_accessor() noexcept = default;
-    
-    template<class OtherElementType>
-    constexpr shared_accessor(shared_accessor<OtherElementType>) noexcept  {};
-    
+
+    template <class OtherElementType>
+    constexpr shared_accessor(shared_accessor<OtherElementType>) noexcept {};
+
     constexpr data_handle_type offset(data_handle_type h, size_t i) const noexcept {
-	return data_handle_type{h.base, h.offset + i};
+        return data_handle_type{h.base, h.offset + i};
     }
-    
+
     constexpr reference access(data_handle_type h, size_t i) const noexcept {
-	return h.base.get()[h.offset + i];
+        return h.base.get()[h.offset + i];
     };
 };
 
-}; // detail
+}; // namespace detail
 
-template<class ElementType,
-	 class Extents,
-	 class LayoutPolicy = Kokkos::layout_right,
-	 class AccessorPolicy = detail::shared_accessor<ElementType>>
-class mdshared : public Kokkos::mdspan<ElementType, Extents, LayoutPolicy, AccessorPolicy> {
-public:
-    using Base = Kokkos::mdspan<ElementType, Extents, LayoutPolicy, AccessorPolicy>;
+template <class ElementType, size_t Rank, std::integral IndexType = int>
+using shared_data = Kokkos::mdspan<ElementType,
+                                   Kokkos::dextents<int, Rank>,
+                                   Kokkos::layout_right,
+                                   detail::shared_accessor<ElementType>>;
 
-    template<std::integral... Args>
-    mdshared(Args... dynamic_extents)
-	: Base(detail::make_shared_ownership_handle<ElementType>
-	       (detail::product_dynamic_extents(dynamic_extents...)),
-	       dynamic_extents...) {
-    }
-};
+template <class ElementType, std::integral... Args>
+auto make_shared_data(Args... dynamic_extents) {
+    return shared_data<ElementType, sizeof...(dynamic_extents)>{
+        detail::make_shared_ownership_handle<ElementType>(
+            detail::product_dynamic_extents(dynamic_extents...)),
+        dynamic_extents...};
+}
 
-template<class ElementType, size_t Rank, std::integral IndexType = int>
-using shared_array = mdshared<ElementType, Kokkos::dextents<IndexType, Rank>>;
+template <class ElementType, std::integral... Args>
+auto make_shared_data(const std::vector<ElementType>& data, Args... dynamic_extents) {
+    auto array = shared_data<ElementType, sizeof...(dynamic_extents)>{
+        detail::make_shared_ownership_handle<ElementType>(
+            detail::product_dynamic_extents(dynamic_extents...)),
+        dynamic_extents...};
+    std::copy(data.begin(), data.end(), begin(element_wise{array}));
+    return array;
+}
 
-}; // core::md
+}; // namespace core::md
--- test/src/core/md/test_md_shared_data.cpp
+++ test/src/core/md/test_md_shared_data.cpp
@@ -1,8 +1,6 @@
 // Copyright (C) 2023, 2024 by Mark Melton
 //
 
-#include <variant>
-#include <array>
 #include <gtest/gtest.h>
 
 #include "core/md/shared.h"
@@ -11,13 +9,23 @@
 using namespace core;
 using ::testing::StaticAssertTypeEq;
 
-TEST(SharedArray, Construct1d) {
-    md::shared_array<int, 1> array(17);
+TEST(SharedArray, Make) {
+    auto array = md::make_shared_data<int>(17);
     EXPECT_EQ(array.extent(0), 17);
     for (auto i = 0; i < array.extent(0); ++i)
 	array[i] = i;
     for (auto i = 0; i < array.extent(0); ++i)
 	EXPECT_EQ(array[i], i);
+}
+
+TEST(SharedArray, MakeFromVec) {
+    std::vector<int> data = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11};
+    auto a = md::make_shared_data<int>(data, data.size());
+    auto b = md::slice(a, md::x{0,20});
+    EXPECT_TRUE(a == b);
+    EXPECT_EQ(a.extent(0), data.size());
+    for (auto i = 0; i < a.extent(0); ++i)
+	EXPECT_EQ(a[i], i);
 }
 
 int main(int argc, char *argv[]) {
